67746fabd3642918fa7e0b0f865d240a0e1a854e
Даниил <mon.inch@yandex.ru>
2024-11-11 19:07:19 +0300

    Сделал теперь, динамическую подгрузку из гугл таблицы, и динамический поиск по json

--- app/api/routers/parser.py
+++ app/api/routers/parser.py
@@ -1,12 +1,25 @@
+from typing import Dict, Optional
 from fastapi import APIRouter, HTTPException, Query
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 from app.api.schemas import SearchQuery
 
-data = pd.read_csv("C:\goal\\table-parser\\test_table.csv")
-data.fillna("Неизвестно", inplace=True)
-data.columns = data.columns.str.replace(" ", "_")
-data.columns = data.columns.str.replace("-", "_")
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "mypython-441415-8a9b22688418.json", scope
+)
+client = gspread.authorize(creds)
+spreadsheet_id = "1-ZwYgQAglAIomCahGYBnwX5PCbFL7a7CqScTA-em7Qk"
+spreadsheet = client.open_by_key(spreadsheet_id)
+sheet = spreadsheet.sheet1
+data = sheet.get_all_records()
+data = pd.DataFrame(data)
 
 router = APIRouter(
     prefix="/parser",
@@ -49,62 +62,14 @@ def get_column_data(
     return column_data
 
 
-# Наименование: Набор 4 банана 6-8 ягод
-# Город: Ижевск
-
-
-@router.post("/search/")
-def search(query: SearchQuery):
+@router.post("/search")
+def search_data(query: Dict[str, Optional[str]]):
     filtered_data = data.copy()
 
-    if query.Наименование:
-        filtered_data = filtered_data[
-            filtered_data["Наименование"].str.contains(
-                query.Наименование, case=False, na=False
-            )
-        ]
-    if query.Город:
-        filtered_data = filtered_data[
-            filtered_data["Город"].str.contains(query.Город, case=False, na=False)
-        ]
-    if query.Категория:
-        filtered_data = filtered_data[
-            filtered_data["Категория"].str.contains(
-                query.Категория, case=False, na=False
-            )
-        ]
-    if query.Для_кого:
-        filtered_data = filtered_data[
-            filtered_data["Для_кого"].str.contains(query.Для_кого, case=False, na=False)
-        ]
-    if query.Размер_упаковки:
-        filtered_data = filtered_data[
-            filtered_data["Размер_упаковки"].str.contains(
-                query.Размер_упаковки, case=False, na=False
-            )
-        ]
-    if query.Шоколад:
-        filtered_data = filtered_data[
-            filtered_data["Шоколад"].str.contains(query.Шоколад, case=False, na=False)
-        ]
-    if query.Наполнение_набора:
-        filtered_data = filtered_data[
-            filtered_data["Наполнение_набора"].str.contains(
-                query.Наполнение_набора, case=False, na=False
-            )
-        ]
-    if query.Ссылка:
-        filtered_data = filtered_data[
-            filtered_data["Ссылка"].str.contains(query.Ссылка, case=False, na=False)
-        ]
+    for key, value in query.items():
+        if key in filtered_data.columns and value:
+            filtered_data = filtered_data[
+                filtered_data[key].str.contains(value, case=False, na=False)
+            ]
 
-    if query.Стоимость is not None:
-        filtered_data = filtered_data[filtered_data["Стоимость"] == query.Стоимость]
-    if query.Кол_во_ягод is not None:
-        filtered_data = filtered_data[filtered_data["Кол_во_ягод"] == query.Кол_во_ягод]
-
-    if filtered_data.empty:
-        raise HTTPException(status_code=404, detail="No matching records found")
-
-    result = filtered_data.to_dict(orient="records")
-    return result
+    return filtered_data.to_dict(orient="records")
