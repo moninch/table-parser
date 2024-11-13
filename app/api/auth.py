16469e10935d2b369d35e272a0b5aea2d57f31a8
Даниил <mon.inch@yandex.ru>
2024-11-13 11:22:58 +0300

    Вынесение логики авторизации в google api

---
+++ app/api/auth.py
@@ -0,0 +1,23 @@
+import gspread_asyncio
+from google.oauth2.service_account import Credentials
+from aiocache import cached
+
+
+def get_google_sheets_client(credentials_file: str):
+    scopes = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    credentials = Credentials.from_service_account_file(credentials_file, scopes=scopes)
+    agcm = gspread_asyncio.AsyncioGspreadClientManager(lambda: credentials)
+    return agcm
+
+
+@cached(ttl=60)
+async def fetch_sheet_data(client_manager, spreadsheet_id: str):
+    async with client_manager as client:
+        spreadsheet = await client.open_by_key(spreadsheet_id)
+        worksheet = await spreadsheet.get_worksheet(0)  # выбор листа
+        records = await worksheet.get_all_records()
+    return records
