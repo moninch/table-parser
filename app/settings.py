ceb9d3d60b31e1da9b4449446a1f7ac7d973b64a
Даниил <mon.inch@yandex.ru>
2024-11-13 11:12:14 +0300

    Оставил только необходимые settings

--- app/settings.py
+++ app/settings.py
@@ -6,19 +6,10 @@ load_dotenv(".env")
 
 
 class Settings(BaseModel):
-    SYSTEM_API_TOKEN: SecretStr
-    BOT_TOKEN: SecretStr
-    MONGODB_URL: SecretStr
-    MONGODB_DB: SecretStr
-    LOGGING_URL: str
     LOGGING_LEVEL: str
-    LOGGING_TOKEN: str
 
     API_PORT: int
     API_HOST: str
-
-    ADMIN_USERNAME: SecretStr
-    ADMIN_PASSWORD: SecretStr
 
 
 SETTINGS = Settings(**environ)
