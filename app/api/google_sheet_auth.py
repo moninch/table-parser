0126d4ae3e3870daa8aa7853091e1a54776addbd
Даниил <mon.inch@yandex.ru>
2024-11-13 15:09:24 +0300

    Обернул в try catch fetch_sheet_data

--- app/api/google_sheet_auth.py
+++ app/api/google_sheet_auth.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import gspread_asyncio
 from google.oauth2.service_account import Credentials
 from aiocache import cached
@@ -16,8 +17,11 @@ def get_google_sheets_client(credentials_file: str):
 
 @cached(ttl=60)
 async def fetch_sheet_data(client_manager, spreadsheet_id: str):
-    client = await client_manager.authorize()
-    spreadsheet = await client.open_by_key(spreadsheet_id)
-    worksheet = await spreadsheet.get_worksheet(0)  # выбор листа
-    records = await worksheet.get_all_records()
+    try:
+        client = await client_manager.authorize()
+        spreadsheet = await client.open_by_key(spreadsheet_id)
+        worksheet = await spreadsheet.get_worksheet(0)  # выбор листа
+        records = await worksheet.get_all_records()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     return records
